76049ed822fe064b16220d43b6b5f45e3ab87d6f
hahakiki2010 <Yanni_smile@hotmail.com>
2016-02-26 12:22:04 +0800

    add ocgas_test_case

--- com/ericsson/xn/x/pm/NBIPmCases/case_all_ne_pm.py
+++ com/ericsson/xn/x/pm/NBIPmCases/case_all_ne_pm.py
@@ -1,21 +1,17 @@
 from com.ericsson.xn.x.pm.PmCommons import NBIPmFunc
 from com.ericsson.xn.commons import test_logger
 from com.ericsson.xn.commons import caseutils
+from com.ericsson.xn.x.pm.NBIPmCases import case_imshss_pm, case_ltehss_pm,\
+    case_mme_pm, case_pgw_pm, case_sgw_pm, case_sgsn_pm, case_ocgas_pm
 
-nes_info = {"SGSN":{"ne_type":"SGSN","nename":"SGSN76","nodeid":"8613743640","licid":"LTE123456789-modify","interval":15,"time":"201602030600"},
-            "MME":{"ne_type":"MME","nename":"MME76","nodeid":"8613743641","licid":"LTE987654321","interval":15,"time":"201602030600"},
-           "SGW":{"ne_type":"SGW","nename":"SGW72","nodeid":"abcd","licid":"ttttt","interval":15,"time":"201602181300"},
-           "PGW":{"ne_type":"PGW","nename":"PGW72","nodeid":"default","licid":"LLLLtest","interval":15,"time":"201602181300"},
-           "AS":{"ne_type":"AS","nename":"ocgas67","nodeid":"1","licid":"","interval":15,"time":"201602010900"},
-           "IMSHSS":{"ne_type":"IMSHSS","nename":"HSSIMS77","nodeid":"HSS_IMS","licid":"","interval":5,"time":"201602181700"},
-           "LTEHSS":{"ne_type":"LTEHSS","nename":"HSSLTE77","nodeid":"HSS_LTE","licid":"","interval":5,"time":"201602181700"}
-           }
-           
 
 def check_pm_all_nes():
-    global nes_info
-    caseutils.pre_test_case("check_nbi_pm_all_nes_cases", "nbi_pm_automation")
-    for ne,info in nes_info.items():
-        test_logger.logger_instance.log_info("Start to check pm data accuracy of " + info["ne_type"])     
-        NBIPmFunc.PMCmpInstance(info["ne_type"],info["nename"],info["nodeid"],info["licid"],info["interval"],info["time"]).check_pm_accuracy()  
-    caseutils.post_test_case()
+    case_imshss_pm.check_pm_imshss()
+    case_ltehss_pm.check_pm_ltehss()
+    case_mme_pm.check_pm_mme()
+    case_sgsn_pm.check_pm_sgsn()
+    case_pgw_pm.check_pm_pgw()
+    case_sgw_pm.check_pm_sgw()
+    case_ocgas_pm.check_pm_as()
+    
+    
