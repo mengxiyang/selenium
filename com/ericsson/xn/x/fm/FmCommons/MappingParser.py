0f04b9b20860e32c8e15dd407ea7b5f23bd6ebb2
hahakiki2010 <Yanni_smile@hotmail.com>
2016-03-23 15:28:52 +0800

    update mappingParser and NotifyDataBase to class

--- com/ericsson/xn/x/fm/FmCommons/MappingParser.py
+++ com/ericsson/xn/x/fm/FmCommons/MappingParser.py
@@ -4,52 +4,48 @@ from com.ericsson.xn.commons import test_logger as test
 import os
 
 
-root_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__))).split('com' + os.sep + 'ericsson' + os.sep + 'xn' + os.sep + 'x' + os.sep + 'fm' + os.sep + 'FmCommons')[0]
-mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'ne_new_alarm.cfg'
 
 
-class MappingParser():
+
+class XMLTree():
 	def __init__(self,mapping_cfg):
 		if not os.path.exists(mapping_cfg):
 			test.error("the mapping.cfg:" + mapping_cfg + " not exist")
-		self.dict_mapping_info = 
-	
-	
+		self.tree = ET.parse(mapping_cfg)
 	
 
-	   def get_children_element(node,map,keys,values):
-	if len(node) == 0:
-		map[node.get("name")] = node.text
-		return map
-	else:
-		continue_nodes=[]
-		for c in node:
-			keys.append(c.get("name"))
-			values.append(c.text)
-			if len(c) != 0:
-				continue_nodes.append(c)
-		map = dict(zip(keys,values))
-		if len(continue_nodes) != 0:
-			for c_c in continue_nodes:
-				map[c_c.get("name")] = get_children_element(c_c,{},[],[])
-		return map
-
-
-def get_element_mapping(tree,names):
-	dict = {}
-	for name in names:
-		node = tree.find(name)
-		if node != None:
-			c_dict = get_children_element(node,{},[],[])
-			dict[name] = c_dict
+	def get_children_element(self,node,map,keys,values):
+		if len(node) == 0:
+			map[node.get("name")] = node.text
+			return map
 		else:
-			test.info("the attribute:" + name + " can't be found in mapping.cfg")
-	return dict
+			continue_nodes=[]
+			for c in node:
+				keys.append(c.get("name"))
+				values.append(c.text)
+				if len(c) != 0:
+					continue_nodes.append(c)
+			map = dict(zip(keys,values))
+			if len(continue_nodes) != 0:
+				for c_c in continue_nodes:
+					map[c_c.get("name")] = self.get_children_element(c_c,{},[],[])
+			return map
 
 
-
-if __name__ == '__main__':
-	tree = ET.parse(mapping_cfg)
-	notif_name = ('event_name','b','w','e')
-	dict_notif = get_element_mapping(tree,notif_name)
-	print dict_notif
+	def get_element_mapping(self,attr_name):
+		dict = {}
+		node = self.tree.find(attr_name)
+		if node != None:
+			c_dict = self.get_children_element(node,{},[],[])
+			dict[attr_name] = c_dict
+		else:
+			test.info("the attribute:" + attr_name + " can't be found in mapping.cfg")
+		return dict
+	
+	
+	def get_all_tags(self):
+		tags=[]
+		root=self.tree.getroot()
+		for element in root.getchildren():
+			tags.append(element.tag)
+		return tags
--- com/ericsson/xn/x/fm/FmCommons/NotifDataBase.py
+++ com/ericsson/xn/x/fm/FmCommons/NotifDataBase.py
@@ -11,18 +11,23 @@ import os
 from com.ericsson.xn.commons import test_logger as test
 from com.ericsson.xn.x.fm.FmCommons.GuiDataBase import alarmMapping
 
+root_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__))).split('com' + os.sep + 'ericsson' + os.sep + 'xn' + os.sep + 'x' + os.sep + 'fm' + os.sep + 'FmCommons')[0]
+notify_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'ne_new_alarm.cfg'
+common_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'common.cfg'
 
-
-class notifMapping():
-    def __init__(self,common_cfg,nbi_mapping_cfg):
+class notifMapping(alarmMapping):
+    
+    def __init__(self,common_cfg,nbi_mapping_cfg):  
+        alarmMapping.__init__(self, common_cfg)
+        m_parser = MappingParser.XMLTree(notify_mapping_cfg) 
+        notif_attributes = m_parser.get_all_tags()
+        for a in notif_attributes:
+            notify_mapping_info = m_parser.get_element_mapping(a)
+        self.notify_mapping_info = dict(self.dict_mapping_info.items() + notify_mapping_info.items())
         
-        self.com_mappingInstance = alarmMapping(common_cfg)
-        self.notif_mappingInstance = MappingParser(nbi_mapping_cfg)
-
-        
-        print self.com_mappingInstance.dict_mapping_info
+    
         
     
             
-    
-
+if __name__ == '__main__':
+    notify = notifMapping(common_mapping_cfg,notify_mapping_cfg)
