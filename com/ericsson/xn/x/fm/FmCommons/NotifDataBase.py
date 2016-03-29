59ef9bbb5284b9f78fbfe910c2c2f3a9f66f28e7
hahakiki2010 <MlvisTong2015>
2016-03-29 10:00:22 +0800

    latest code 0329

--- com/ericsson/xn/x/fm/FmCommons/NotifDataBase.py
+++ com/ericsson/xn/x/fm/FmCommons/NotifDataBase.py
@@ -11,20 +11,16 @@ import os
 from com.ericsson.xn.commons import test_logger as test
 from com.ericsson.xn.x.fm.FmCommons.GuiDataBase import alarmMapping
 from com.ericsson.xn.x.fm.FmCommons.NotifFunc import compare_data
+from com.ericsson.xn.x.fm.FmCommons.NotifFunc import get_nodeid_from_db
 
 root_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__))).split('com' + os.sep + 'ericsson' + os.sep + 'xn' + os.sep + 'x' + os.sep + 'fm' + os.sep + 'FmCommons')[0]
-notify_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'ne_new_alarm.cfg'
-common_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'common.cfg'
+notify_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'hss_new_alarm.cfg'
 
 class notifMapping(alarmMapping):
     
-    def __init__(self,common_cfg,nbi_mapping_cfg):  
-        alarmMapping.__init__(self, common_cfg)
-        m_parser = MappingParser.XMLTree(notify_mapping_cfg) 
-        notif_attributes = m_parser.get_children_tags()
-        for a in notif_attributes:
-            notify_mapping_info = m_parser.get_element_mapping(a)
-        self.notify_mapping_info = dict(self.dict_mapping_info.items() + notify_mapping_info.items())
+    def __init__(self,nbi_mapping_cfg):
+        alarmMapping.__init__(self, nbi_mapping_cfg)
+        self.notify_mapping_info = self.dict_mapping_info
         
     
     def get_property(self, key):
@@ -32,48 +28,3 @@ class notifMapping(alarmMapping):
             return self.notify_mapping_info[key]
         else:
             test.failed("get " + key + " from mapping Failed")
-                
-    
-    def check_attr_accuacy(self,alarm_trap,dict_nbi_notif,nename,*attrs):
-        attr_list = attrs
-        for a in attrs:
-            expected_value = {}
-            if 'event_name' == a:    
-                if dict_nbi_notif.has_key('event_name'):
-                    notif_value = dict_nbi_notif['event_name']
-                    mapped_event_name = self.convert_event_type(alarm_trap['alarmLevel'])
-                    expected_value['event_name'] = '"' + mapped_event_name + '"'
-                    compare_data(notif_value,expected_value)
-                else:
-                    test.failed("get 'event_name' from nbi notification Failed")
-                
-            elif "event_type" == a:
-                if dict_nbi_notif.has_key("event_type"):
-                    notif_value = dict_nbi_notif["event_type"]
-                    expected_value = {'event_type':{'null':{'domain_name':'Alarm IRP V3.0.0','type_name':'x1'}}}
-                    compare_data(notif_value, expected_value)
-                else:
-                    test.failed("get 'event_type' from nbi notification Failed")
-            
-            elif 'd'  == a:
-                if dict_nbi_notif.has_key("d"):
-                    notif_value = dict_nbi_notif["d"]
-                    expected_value = {'d':{'value':{'CORBA::String':'"ManagementNode"'}}}
-                    compare_data(notif_value, expected_value)
-                else:
-                    test.failed("get 'd' from nbi notification Failed")
-                    
-            elif "e" == a:
-                if dict_nbi_notif.has_key["e"]:
-                    notif_value = dict_nbi_notif["e"]
-                    if self.dict_mapping_info["object_class"] == 'ManagedElement':
-                        nodeid = get_nodeid_from_db(nename)
-                        dn = 'DC=Ericsson,SubNetwork=1,ManagedElement=' + nodeid + '|' + nename
-                        expected_value = {'e':{'value':{'CORBA::String':'"' + dn + '"'}}
-                
-                    
-        
-    
-            
-if __name__ == '__main__':
-    notify = notifMapping(common_mapping_cfg,notify_mapping_cfg)
--- com/ericsson/xn/x/fm/FmCommons/NotifFunc.py
+++ com/ericsson/xn/x/fm/FmCommons/NotifFunc.py
@@ -8,11 +8,17 @@ from com.ericsson.xn.commons import test_logger as test
 from com.ericsson.xn.commons.base_clint_for_selenium import send_trap
 from com.ericsson.xn.commons.PyProperties import Properties
 from com.ericsson.xn.x.fm.TestCases.case_ocgas_fm import server_info_cfg
-from com.ericsson.xn.x.fm.FmCommons.FmCommon import data_init, query_alarm
+from com.ericsson.xn.x.fm.FmCommons.FmCommon import data_init, query_alarm,quitDriver
 from com.ericsson.xn.commons import CommonStatic
 import types
 
-    
+root_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__))).split('com' + os.sep + 'ericsson' + os.sep + 'xn' + os.sep + 'x' + os.sep + 'fm' + os.sep + 'FmCommons')[0]
+notify_mapping_cfg = root_dir + os.sep + 'x' + os.sep + 'fm' + os.sep + 'nbi_mapping' + os.sep + 'hss_new_alarm.cfg'
+server_info_cfg = root_dir + os.sep + 'x' + os.sep + 'pm' + os.sep + 'execute_conf.cfg'
+ne_info_cfg = root_dir + os.sep + 'x' + os.sep + 'pm' + os.sep + 'nes' + 'imshss.cfg'
+
+
+
 def compare_data(notif_ne, expected_result):
     for key,value in expected_result.item():
         if notif_ne.has_key(key):
@@ -35,18 +41,65 @@ def compare_data(notif_ne, expected_result):
             
 
 def get_nodeid_from_db(nename):
+	return '12345'
                        
     
-def getNBINotification(basemgr_ip,basemgr_port,basemgr_pwd,ne_type,alarm_type,host_ip):
+def getNBINotification(basemgr_ip,basemgr_port,basemgr_pwd,ne_type,alarm_type,host_ip,snmp_auth_info):
+	return {'error_code':'1','msg':'get nbi notification successfully','nbi':{'event_name':'x1','event_type':{'null':{'domain_name':'Alarm IRP V3.0.0','type_name':type_name}}},'trap':{'alarmCategory':'CommunicationsAlarm'}}
     
-def check_notify__data_accuracy(ne_info_cfg,server_info_cfg,mapping_info_cfg,common_cfg):
-    dict_ne_info,dict_server_info,dict_browser_chrome = data_init(ne_info_cfg,server_info_cfg,mapping_info_cfg,common_cfg)
-    driver = CommonStatic.login_rsnms(dict_browser_chrome,dict_server_info["host"],dict_server_info["username"],dict_server_info["password"],dict_server_info["port"],dict_server_info["url"])
+
+def check_attr_accuracy(mappingInstance,alarm_trap,dict_nbi_notif,nename,*attrs):
+    attr_list = attrs
+    for a in attrs:
+        expected_value = {}
+        if 'event_name' == a:
+            if dict_nbi_notif.has_key('event_name'):
+                notif_value = dict_nbi_notif['event_name']
+                mapped_event_name = mappingInstance.convert_event_type(alarm_trap['alarmCategory'])
+                expected_value['event_name'] = '"' + mapped_event_name + '"'
+                compare_data(notif_value,expected_value)
+            else:
+                test.failed("get 'event_name' from nbi notification Failed")
+
+        elif "event_type" == a:
+            if dict_nbi_notif.has_key("event_type"):
+                notif_value = dict_nbi_notif["event_type"]
+                type_name = "'" + mappingInstance.notify_mapping_info["type_name"] + "'"
+                expected_value = {'event_type':{'null':{'domain_name':'Alarm IRP V3.0.0','type_name':type_name}}}
+                compare_data(notif_value, expected_value)
+            else:
+                test.failed("get 'event_type' from nbi notification Failed")
+
+        elif 'd'  == a:
+            if dict_nbi_notif.has_key("d"):
+                notif_value = dict_nbi_notif["d"]
+                object_class = "'" + mappingInstance.notify_mapping_info['object_class'] + "'"
+                expected_value = {'d':{'value':{'CORBA::String':object_class}}}
+                compare_data(notif_value, expected_value)
+            else:
+                test.failed("get 'd' from nbi notification Failed")
+
+        elif "e" == a:
+            if dict_nbi_notif.has_key["e"]:
+                notif_value = dict_nbi_notif["e"]
+                if mappingInstance.notify_mapping_info["object_class"] == 'ManagedElement':
+                    nodeid = get_nodeid_from_db(nename)
+                    dn = 'DC=Ericsson,SubNetwork=1,ManagedElement=' + str(nodeid) + '|' + nename
+                elif mappingInstance.notify_mapping_info["object_class"] == 'ManagedNode':
+                    dn = 'DC=Ericsson,SubNetwork=1,ManagedNode=1'
+                expected_value = {'e':{'value':{'CORBA::String':'"' + dn + '"'}}}
+                compare_data(notif_value,expected_value)
+
+def check_notify__data_accuracy(ne_info_cfg,server_info_cfg,mapping_info_cfg):
+    dict_ne_info,dict_server_info,dict_browser_chrome = data_init(ne_info_cfg,server_info_cfg)
+    #driver = CommonStatic.login_rsnms(dict_browser_chrome,dict_server_info["host"],dict_server_info["username"],dict_server_info["password"],dict_server_info["port"],dict_server_info["url"])
+    driver = True
     if driver:
         try:
-            NeCommon.to_ne_management_page_by_url(driver,server_info)
-            new_ne_info=NeCommon.check_and_add_ne(driver, dict_ne_info)
-            ne_name = new_ne_info["ne_name"]
+            #NeCommon.to_ne_management_page_by_url(driver,server_info)
+            #new_ne_info=NeCommon.check_and_add_ne(driver, dict_ne_info)
+            #ne_name = new_ne_info["ne_name"]
+            ne_name = "IMSHSS77"
             if dict_ne_info["ne_type"] == "LTEHSS" or dict_ne_info["ne_type"] == "IMSHSS":
                 snmp_auth_info = []
                 snmp_auth_info.append(dict_ne_info["usm_user"])
@@ -55,18 +108,28 @@ def check_notify__data_accuracy(ne_info_cfg,server_info_cfg,mapping_info_cfg,com
             else:
                 snmp_auth_info = []
                 
-            FmCommon.toAlarmManagement_by_url(driver,dict_server_info)
-            FmCommon.init_and_search(driver,ne_name)
-            mappingInstance = notifMapping(common_cfg)
+            #FmCommon.toAlarmManagement_by_url(driver,dict_server_info)
+            #FmCommon.init_and_search(driver,ne_name)
+            mappingInstance = notifMapping(mapping_info_cfg)
             alarm_type_list = mappingInstance.dict_mapping_info["alarm_types"]
             for alarm_type in alarm_type_list:
                 test.info("send and get NBI notification: " + dict_ne_info["ne_type"] + ":" + alarm_type + "...")
                 alarm_raw = getNBINotification(dict_ne_info["ne_ip"], 7070, 'xoambaseserver',dict_ne_info["ne_type"],alarm_type,dict_server_info["host"],snmp_auth_info)
                 error_code = int(alarm_raw["code"])
                 if error_code==1:
-                    query_alarm(driver)
+                    #query_alarm(driver)
                     alarm_trap = alarm_raw["trap"]
                     nbi_notif = alarm_raw["nbi"]
                     test.info("send and get NBI notification successfully.TrapInfo:" + str(alarm_trap) + " and NotifInfo:" + str(nbi_notif))
                     check_notif_items = tuple(mappingInstance.dict_mapping_info("notif_attr_names"))
-                    mappingInstance.check_attr_accuacy(alarm_trap,nbi_notif,dict_ne_info['nename'],check_notif_items)
+                    mappingInstance.check_attr_accuracy(mappingInstance,alarm_trap,nbi_notif,ne_name,check_notif_items)
+        except Exception as e:
+            #quitDriver(driver)
+            test.error(e.message)
+
+
+if __name__ == '__main__':
+    check_notify__data_accuracy(ne_info_cfg,server_info_cfg,notify_mapping_cfg)
+
+
+
