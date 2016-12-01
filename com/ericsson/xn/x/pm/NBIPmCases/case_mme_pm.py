198938575c825e28564e34c5881226f1bd3252e0
hahakiki2010 <Yanni_smile@hotmail.com>
2016-12-01 16:13:01 +0800

    update case_mme_pm.py

--- com/ericsson/xn/x/pm/NBIPmCases/case_mme_pm.py
+++ com/ericsson/xn/x/pm/NBIPmCases/case_mme_pm.py
@@ -2,10 +2,11 @@ from com.ericsson.xn.x.pm.PmCommons import NBIPmFunc
 from com.ericsson.xn.commons import test_logger
 from com.ericsson.xn.commons import caseutils
 
-nes_info =  {"MME":{"ne_type":"MME","nename":"MME76","nodeid":"8613743641","licid":"LTE987654321","interval":15,"time":"201602030600"}}
+nes_info =  {"MME":{"ne_type":"MME","nename":"MME90","nodeid":"8613743641","licid":"LTE987654321","interval":15,"time":"201608041030"}}
 
 def check_pm_mme():
     global nes_info
     caseutils.pre_test_case("check_nbi_pm_"+nes_info["MME"]["ne_type"]+"_rop="+str(nes_info["MME"]["interval"])+"_cases", "nbi_pm_automation")
     NBIPmFunc.PMCmpInstance(nes_info["MME"]["ne_type"],nes_info["MME"]["nename"],nes_info["MME"]["nodeid"],nes_info["MME"]["licid"],nes_info["MME"]["interval"],nes_info["MME"]["time"]).check_pm_accuracy()  
     caseutils.post_test_case()
+    
